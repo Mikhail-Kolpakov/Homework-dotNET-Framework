64669071cb6c21e64c1c64d3875e0b138c5ddfd4
Mikhail-Kolpakov <mihailkolp@gmail.com>
2024-03-01 21:31:16 +0200

    Lab. 7

---
+++ Lab7/Ex1/Ex1/Program.cs
@@ -0,0 +1,58 @@
+﻿using System.Text;
+
+Console.OutputEncoding = Encoding.UTF8;
+var aircraftCarriers = new[]
+{
+    new AircraftCarrier("Гетьман Іван Мазепа", "проведення широкомасштабних морських операцій у зоні підвищеного ризику", 70000, 150000, "дизель", "Ка-27", 35),
+    new AircraftCarrier("Фортеця Львів", "контроль за морськими кордонами та боротьба з нелегальними морськими активностями", 40000, 180000, "ядерне паливо", "MQ-9 Reaper", 45),
+    new AircraftCarrier("Незламний", "гуманітарні операції та медичні місії", 60000, 120000, "авіагаз", "E-2 Hawkeye", 20),
+    new AircraftCarrier("Запорізька Січ", "підтримка морської безпеки та захисту національних морських коридорів", 80000, 200000, "дизель", "CH-47 Chinook", 35),
+    new AircraftCarrier("Левітан", "проведення розвідувальних та антитерористичних операцій у різних частинах світу", 75000, 160000, "природній газ", "F-35B Lightning II", 30)
+};
+
+var missileCarriers = new[]
+{
+    new MissileCarrier("Українська Стріла", "забезпечення оборони країни від потенційних загроз", 50000, 100000, "керосин", "SM-3", 40),
+    new MissileCarrier("Дніпровський Гром", "забезпечення міжнародної стабільності та врегулювання конфліктів", 55000, 120000, "керосин", "Tomahawk", 16),
+    new MissileCarrier("Гайдамацька Вітрила", "забезпечення повітряної безпеки та ведення повітряної боротьби", 40000, 90000, "тверде паливо", "AIM-120 AMRAAM", 20),
+    new MissileCarrier("Славута-М", "підтримка повстанців або опозиційних сил у різних країнах", 60000, 140000, "керосин", "Harpoon", 32),
+    new MissileCarrier("Карпатська Скеля", "підтримка сил міжнародного співробітництва та обміну інформацією", 52000, 110000, "гібридне паливо", "AGM-158 JASSM", 26)
+};
+
+Console.WriteLine("Авіаносці: ");
+PrintInfo(aircraftCarriers);
+
+Console.WriteLine("Ракетоносії: ");
+PrintInfo(missileCarriers);
+
+return;
+
+void PrintInfo(IEnumerable<Ship> ships)
+{
+    foreach (var ship in ships)
+    {
+        Console.WriteLine($"Назва: {ship.Name}");
+        Console.WriteLine($"Призначення: {ship.Purpose}");
+        Console.WriteLine($"Водотоннажність: {ship.WaterContent} т");
+        Console.WriteLine($"Потужність двигуна: {ship.EnginePower} кіловатт");
+        Console.WriteLine($"Тип палива: {ship.FuelType}");
+        switch (ship)
+        {
+            case AircraftCarrier aircraftCarrier:
+                Console.WriteLine($"Тип літаків: {aircraftCarrier.AircraftType}");
+                Console.WriteLine($"Кількість літаків: {aircraftCarrier.AircraftCount}");
+                break;
+            case MissileCarrier missileCarrier:
+                Console.WriteLine($"Тип ракет: {missileCarrier.MissilesType}");
+                Console.WriteLine($"Кількість ракет: {missileCarrier.MissilesCount}");
+                break;
+        }
+        Console.WriteLine();
+    }
+}
+
+internal record Ship(string Name, string Purpose, float WaterContent, float EnginePower, string FuelType);
+internal record AircraftCarrier(string Name, string Purpose, float WaterContent, float EnginePower, string FuelType,
+    string AircraftType, int AircraftCount) : Ship(Name, Purpose, WaterContent, EnginePower, FuelType);
+internal record MissileCarrier(string Name, string Purpose, float WaterContent, float EnginePower, string FuelType, 
+    string MissilesType, int MissilesCount) : Ship(Name, Purpose, WaterContent, EnginePower, FuelType);
---
+++ Lab7/Ex2/Ex2/Program.cs
@@ -0,0 +1,45 @@
+﻿// Варіант #9
+using System.Text;
+
+Console.OutputEncoding = Encoding.UTF8;
+
+Toy toy = new Toy("Ведмедик Фрайді", 15.99, 3);
+toy.Display();
+
+DairyProduct milk = new DairyProduct("Молоко", 2.49, DateTime.Now.AddDays(7));
+milk.Display();
+
+// Базовий клас Товар
+internal class Goods(string name, double price)
+{
+    protected string Name { get; } = name;
+    protected double Price { get; } = price;
+
+    public virtual void Display() =>
+        Console.WriteLine($"Назва: {Name}, Ціна: {Price}$");
+}
+
+// Похідний клас Продукт
+internal class Product(string name, double price) : Goods(name, price)
+{
+    public override void Display() =>
+        Console.WriteLine($"Продукт - Назва: {Name}, Ціна: {Price}$");
+}
+
+// Похідний клас Іграшка
+internal class Toy(string name, double price, int ageRestriction) : Product(name, price)
+{
+    private int AgeRestriction { get; } = ageRestriction;
+
+    public override void Display() =>
+        Console.WriteLine($"Іграшка - Назва: {Name}, Ціна: {Price}$, Вікове обмеження: {AgeRestriction}+ роки");
+}
+
+// Похідний клас Молочний Продукт
+internal class DairyProduct(string name, double price, DateTime expiryDate) : Product(name, price)
+{
+    private DateTime ExpiryDate { get; } = expiryDate;
+
+    public override void Display() =>
+        Console.WriteLine($"Молочний продукт - Назва: {Name}, Ціна: {Price}$, Вжити до: {ExpiryDate.ToShortDateString()}");
+}
