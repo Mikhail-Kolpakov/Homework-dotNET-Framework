51afe26cd86b527db2a6b99414bfa70735e5333b
Mikhail-Kolpakov <mihailkolp@gmail.com>
2024-03-02 14:32:40 +0200

    Lab. 9

---
+++ Lab9/Ex1/Ex1/Program.cs
@@ -0,0 +1,39 @@
+﻿using System.Text;
+
+Console.OutputEncoding = Encoding.UTF8;
+
+var cargo = new CargoTransport(2000, 1000, 5000);
+var passenger = new PassengerTransport(1500, 800, 50);
+
+Console.WriteLine($"Прибуток вантажного транспорту: {cargo.CalculateProfit()}$");
+Console.WriteLine($"Прибуток пасажирського транспорту: {passenger.CalculateProfit()}$");
+
+// Базовий клас Транспорт
+internal class Transport(float income, float expenses)
+{
+    private float Income { get; } = income; // Прибуток
+    private float Expenses { get; } = expenses; // Витрати
+
+    public virtual float CalculateProfit() =>
+        Income - Expenses;
+}
+
+// Похідний клас Вантажний транспорт
+internal class CargoTransport(float income, float expenses, float cargoWeight) : Transport(income, expenses)
+{
+    private float CargoWeight { get; } = cargoWeight; // Вага вантажу
+
+    // Метод для розрахунку прибутку з урахуванням ваги вантажу
+    public override float CalculateProfit() =>
+        base.CalculateProfit() - CargoWeight * 0.1F;
+}
+
+// Похідний клас Пасажирський транспорт
+internal class PassengerTransport(float income, float expenses, int numberOfPassengers) : Transport(income, expenses)
+{
+    private int NumberOfPassengers { get; } = numberOfPassengers; // Кількість пасажирів
+
+    // Метод для розрахунку прибутку з урахуванням кількості пасажирів
+    public override float CalculateProfit() =>
+        base.CalculateProfit() + NumberOfPassengers * 5;
+}
---
+++ Lab9/Ex2/Ex2/Program.cs
@@ -0,0 +1,43 @@
+﻿using System.Text;
+
+Console.OutputEncoding = Encoding.UTF8;
+
+var rectangle = new Rectangle(5, 4);
+var circle = new Circle(3);
+
+Console.WriteLine($"Периметр прямокутника: {rectangle.CalculatePerimeter()} см");
+Console.WriteLine($"Площа прямокутника: {rectangle.CalculateArea()} см^2");
+Console.WriteLine($"\nДовжина кола: {circle.CalculatePerimeter():f2} см");
+Console.WriteLine($"Площа кола: {circle.CalculateArea():f2} см^2");
+
+// Базовий абстрактиний клас Фігура
+internal abstract class Shape
+{
+    public virtual float CalculatePerimeter() => 0;
+    public virtual float CalculateArea() => 0;
+}
+
+// Похідний клас Прямокутник
+internal class Rectangle(float length, float width) : Shape
+{
+    private float Length { get; } = length;
+    private float Width { get; } = width;
+
+    public override float CalculatePerimeter() =>
+        2 * (Length + Width);
+    
+    public override float CalculateArea() =>
+        Length * Width;
+}
+
+// Похідний клас Коло
+internal class Circle(float radius) : Shape
+{
+    private float Radius { get; } = radius;
+
+    public override float CalculatePerimeter() =>
+        (float)(2 * Math.PI * Radius);
+    
+    public override float CalculateArea() =>
+        (float)(Math.PI * Math.Pow(Radius, 2));
+}
