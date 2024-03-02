ccf65c3c4e7cc0646c8b5a9a1a43459f19bdc523
Mikhail-Kolpakov <mihailkolp@gmail.com>
2024-03-02 23:27:16 +0200

    Lab. 10

---
+++ Lab10/Ex1/Ex1/Program.cs
@@ -0,0 +1,76 @@
+﻿using System.Text;
+
+Console.OutputEncoding = Encoding.UTF8;
+
+var fighterJet = new FighterJet();
+fighterJet.InputData();
+fighterJet.DisplayData();
+
+Console.WriteLine();
+
+var passengerLiner = new PassengerLiner();
+passengerLiner.InputData();
+passengerLiner.DisplayData();
+
+internal abstract class Aircraft
+{
+    protected string? Model { get; set; }
+    protected int Year { get; set; }
+
+    protected T Input<T>(string prompt)
+    {
+        T result;
+    
+        Console.Write(prompt);
+        if (typeof(T) == typeof(string))
+            result = (T)Convert.ChangeType(Console.ReadLine()!, typeof(T));
+        else if (typeof(T) == typeof(int))
+        {
+            int number;
+        
+            while (!int.TryParse(Console.ReadLine(), out number))
+                Console.WriteLine(string.Join(" ", "Помилка: введіть коректне значення.", prompt));
+        
+            result = (T)Convert.ChangeType(number, typeof(T));
+        }
+        else
+            throw new NotSupportedException("Тип даних не підтримується.");
+
+        return result;
+    }
+    
+    public abstract void InputData();
+    public abstract void DisplayData();
+}
+
+internal class FighterJet : Aircraft
+{
+    private int MaxSpeed { get; set; }
+
+    public override void InputData()
+    {
+        Console.WriteLine("Введіть характеристики винищувача: ");
+        Model = Input<string>("Модель: ");
+        Year = Input<int>("Рік: ");
+        MaxSpeed = Input<int>("Максимальна швидкість (км/год): ");
+    }
+
+    public override void DisplayData() =>
+        Console.WriteLine($"Винищувач - Модель: {Model}, Рік: {Year}, Максимальна швидкість: {MaxSpeed} км/год");
+}
+
+internal class PassengerLiner : Aircraft
+{
+    private int PassengerCapacity { get; set; }
+
+    public override void InputData()
+    {
+        Console.WriteLine("Введіть характеристики пасажирського лайнера: ");
+        Model = Input<string>("Модель: ");
+        Year = Input<int>("Рік: ");
+        PassengerCapacity = Input<int>("Пасажировмісткість: ");
+    }
+
+    public override void DisplayData() =>
+        Console.WriteLine($"Пасажирський лайнер - Модель: {Model}, Рік: {Year}, Пасажировмісткість: {PassengerCapacity}");
+}
---
+++ Lab10/Ex2/Ex2/Program.cs
@@ -0,0 +1,36 @@
+﻿using System.Text;
+
+Console.OutputEncoding = Encoding.UTF8;
+
+var car = new Car("Toyota", "Corolla", 2020);
+car.DisplayInfo();
+
+var bicycle = new Bicycle("Mountain Bike", 2019);
+bicycle.DisplayInfo();
+
+// Абстрактний клас Транспортний Засіб
+internal abstract class TransportVehicle(int year)
+{
+    protected int Year { get; } = year;
+    
+    public abstract void DisplayInfo();
+}
+
+// Клас Автомобіль
+internal class Car(string brand, string model, int year) : TransportVehicle(year)
+{
+    private string Brand { get; } = brand;
+    private string Model { get; } = model;
+    
+    public override void DisplayInfo() =>
+        Console.WriteLine($"Автомобіль: {Brand} {Model}, Рік: {Year}");
+}
+
+// Клас Велосипед
+internal class Bicycle(string type, int year) : TransportVehicle(year)
+{
+    private string Type { get; } = type;
+    
+    public override void DisplayInfo() =>
+        Console.WriteLine($"Вид велосипеду: {Type}, Рік: {Year}");
+}
